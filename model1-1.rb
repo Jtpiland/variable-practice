e51ebc87bbd83a8bcefc45c76f1fb2b2844d13d6
Jacob Piland <piland.jacob@gmail.com>
2021-03-31 18:10:49 -0500

    add variables:integers and floats

--- model1-1.rb
+++ model1-1.rb
@@ -1,3 +1,12 @@
+# Variables assigned to strings
 name = Jacob
 partner = Courtney
 pet = Clif
+
+# Variables assigned to integers
+favorite_number = 11
+birthyear = 1987
+
+# Variables assigned to floats
+pi = 3.14
+height = 6.5 #(ft)
