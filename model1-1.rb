6b62ad33e98b590f3b62440a837c17e3e5c4a746
Jacob Piland <piland.jacob@gmail.com>
2021-03-31 18:30:24 -0500

    edit all to model Jacob

--- model1-1.rb
+++ model1-1.rb
@@ -1,24 +1,24 @@
 # Variables assigned to strings
 name = "Jacob"
-partner = "Courtney"
-pet = "Clif"
+his_partner = "Courtney"
+his_pet = "Clif"
 
 # Variables assigned to integers
-favorite_number = 11
-birthyear = 1987
+his_favorite_number = 11
+his_birthyear = 1987
 
 # Variables assigned to floats
-pi = 3.14
-height = 6.5 #(ft)
+pi_land = 3.14 #my last name
+his_height = 6.083 #(ft)
 
 #Variables that are assigned to booleans
 has_hair = false
 is_bald = true
 
 # Variables that are assigned to arrays
-vacations = ["Vermont","Asheville","Mexico"]
-guitars = ["Paul Reed Smith", "Fender", "Collings"]
+his_past_vacations = ["Vermont","Asheville","Mexico"]
+his_guitars = ["Paul Reed Smith", "Fender", "Seagull"]
 
 # Variables that are assigned to Hash
-fruits = {orange: 1, lemon: 2, lime: 3}
-bikes = {surly: 1, fairdale: 2, allcity: 3}
+his_favorite_fruits = {orange: 1, apple: 2, blueberries: 3}
+his_favorite_bike = {surly: 1, fairdale: 2, allcity: 3}
